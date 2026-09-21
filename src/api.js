528dcc2beaffadbfaa7c6b46b71f715a11a073a1
Rykelmy V. Belo Senai <rykelmy.belo@aluno.senai.br>
2026-09-21 09:57:38 -0300

    //Produção

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://taskflow-api-rykelmy.vercel.app",
+  baseURL: import.meta.env.VITE_BASE_URL_API,
 });
 
 api.interceptors.request.use((config) => {
